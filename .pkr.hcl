945e779b9e09fd4b75e8aea07fb96339792117a0
Alex Rad <alex@supernetworks.org>
2024-06-25 20:23:40 -0700

    Update to noble image

--- .pkr.hcl
+++ .pkr.hcl
@@ -1,7 +1,7 @@
 packer {
   required_plugins {
     digitalocean = {
-      version = ">= 1.0.4"
+      version = ">= 1.3.0"
       source  = "github.com/digitalocean/digitalocean"
     }
   }
--- scripts/019-spr.sh
+++ scripts/019-spr.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+set -x
+install_deps() {
+        # install upstream docker
+        apt-get -y install ca-certificates curl gnupg
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+        chmod a+r /etc/apt/keyrings/docker.gpg
+
+        echo \
+                "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+                tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        apt update
+        apt-get -y install --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+apt-get update
+apt-get upgrade
+install_deps
 
 SPR_HOME=/home/spr
 mkdir $SPR_HOME 2>/dev/null
@@ -7,14 +27,14 @@ cd $SPR_HOME
 git clone https://github.com/spr-networks/super.git
 cd super
 #cp docker-compose-virt.yml docker-compose.yml
-docker-compose -f docker-compose-virt.yml pull # update latest
+docker compose -f docker-compose-virt.yml pull # update latest
 #./pull_containers.sh
 
 dd if=/dev/zero of=/swapfile bs=1024 count=256k
-mkswap /swapfile
-#sudo swapon /swapfile
 chown root:root /swapfile
 chmod 0600 /swapfile
+mkswap /swapfile
+#sudo swapon /swapfile
 
 echo "/swapfile       none    swap    sw      0       0" >>/etc/fstab
 
